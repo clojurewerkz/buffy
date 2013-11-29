d428e8251df445e7cee53e7652195c8d32e5a08f
Alex Petrov <alexp@coffeenco.de>
2013-11-29 23:02:29 +0100

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/buffy "0.3.0"
+(defproject clojurewerkz/buffy "0.4.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
