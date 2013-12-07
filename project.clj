7be609c987c2ce2cbae89ef8d0334f639e7da076
Alex Petrov <alexp@coffeenco.de>
2013-12-07 14:22:08 +0100

    Make travis builds pass

--- project.clj
+++ project.clj
@@ -6,4 +6,12 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [io.netty/netty-buffer "4.0.12.Final"]
                  [reiddraper/simple-check "0.5.3"]]
-  :profiles    {:dev {:resource-paths ["resources"]}})
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :dev {:resource-paths ["test/resources"]
+                   :dependencies   [[com.lmax/disruptor "3.2.0"]]
+                   :plugins [[codox "0.6.4"]]
+                   :codox {:sources ["src/clojure"]
+                           :output-dir "doc/api"}}}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]})
