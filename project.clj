0fcecc82cadf5d93c41d3ec0e404244004bc7e27
Alex Petrov <alexp@coffeenco.de>
2013-12-07 14:22:26 +0100

    Release 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/buffy "0.5.0-SNAPSHOT"
+(defproject clojurewerkz/buffy "0.5.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
