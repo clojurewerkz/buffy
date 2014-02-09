b6bd747fd8d4e50bfe386610a59a1a72e76468ad
Michael Klishin <michael@clojurewerkz.org>
2014-02-09 20:31:51 +0400

    Ditto

--- src/clojurewerkz/buffy/types.clj
+++ src/clojurewerkz/buffy/types.clj
@@ -1,3 +1,12 @@
+;; Copyright (c) 2013-2014 Alex Petrov, Michael S. Klishin, and the ClojureWerkz Team
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns clojurewerkz.buffy.types
   (:refer-clojure :exclude [read])
   (:require [clojurewerkz.buffy.util :refer :all]
