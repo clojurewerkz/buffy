a688a0ffe0d9599a2bedfe4a672f9b63504ffbc4
Michael Klishin <michael@clojurewerkz.org>
2014-04-20 00:23:46 +0400

    1.5 is an extra profile now

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
                  [io.netty/netty-buffer "4.0.18.Final"]
                  [reiddraper/simple-check "0.5.3"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.6.7"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev"]})
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev"]})
