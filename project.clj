960273e472c09d83341309314ceff851fad51956
Michael Klishin <michael@clojurewerkz.org>
2014-06-01 15:47:03 +0400

    Codox 0.8.10

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev {:resource-paths ["test/resources"]
-                   :plugins [[codox "0.6.7"]]
+                   :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
   :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev"]})
