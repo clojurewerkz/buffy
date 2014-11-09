ea495717ba29fee5c1c2e4be7353588df1296c8a
Michael Klishin <michael@clojurewerkz.org>
2014-11-09 03:57:08 +0300

    Drop 1.4 test profile

--- project.clj
+++ project.clj
@@ -6,12 +6,11 @@
   :dependencies [[org.clojure/clojure     "1.6.0"]
                  [io.netty/netty-buffer   "4.0.21.Final"]
                  [reiddraper/simple-check "0.5.3"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,1.7:dev:dev,master"]})
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev:dev,master"]})
