84dfe9496164c69e81d2f612d86a641b18fee34c
Michael Klishin <michael@clojurewerkz.org>
2014-11-09 03:56:11 +0300

    Test against Clojure 1.7

--- project.clj
+++ project.clj
@@ -8,8 +8,9 @@
                  [reiddraper/simple-check "0.5.3"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev"]})
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,1.7:dev"]})
