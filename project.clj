a825b8a203aa31f4afe967722e9f57c685871a5f
Michael Klishin <michael@clojurewerkz.org>
2015-07-12 01:04:47 +0300

    Test against Clojure 1.7.0 GA

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 ;                 "-javaagent:/Users/ifesdjeen/hackage/introspect/target/introspect-1.0.0-SNAPSHOT-standalone.jar=clojurewerkz.buffy.core"
 ;]
 
-  :profiles     {:1.7    {:dependencies [[org.clojure/clojure "1.7.0-RC2"]]}
+  :profiles     {:1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
                  :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
                  :dev    {:resource-paths ["test/resources"]
                           :plugins [[codox "0.8.10"]]
